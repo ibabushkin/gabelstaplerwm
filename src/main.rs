cc6cedac1475828e662278c218b354d7f964c5f7
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-04-06 18:15:02 +0200

    Cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,33 @@ use xcb::*;
 
 // an error encountered by the WM
 enum WmError {
-    //CouldNotConnect(ConnError),
+    CouldNotConnect(ConnError),
     CouldNotAcquireScreen,
-    CouldNotRegisterAtom,
+    CouldNotRegisterAtom(String),
     OtherWmRunning,
+    ConnectionInterrupted,
+    IOError
+}
+
+impl WmError {
+    // handle an error, ie. print error message and exit
+    pub fn handle(self) -> ! {
+        match self {
+            WmError::CouldNotConnect(e) =>
+                println!("Could not connect: {:?}", e),
+            WmError::CouldNotAcquireScreen =>
+                println!("Could not acquire screen."),
+            WmError::CouldNotRegisterAtom(s) =>
+                println!("Could not register atom. {}", s),
+            WmError::OtherWmRunning =>
+                println!("Another WM is running."),
+            WmError::ConnectionInterrupted =>
+                println!("Connection interrupted."),
+            WmError::IOError =>
+                println!("IO error occured.")
+        };
+        exit(1);
+    }
 }
 
 // a window manager, wrapping a Connection and a root window
@@ -23,11 +46,11 @@ impl<'a> Wm<'a> {
     pub fn new(con: &'a Connection, screen_num: i32)
         -> Result<Wm<'a>, WmError> {
         let setup = con.get_setup();
-        let screen = match setup.roots().nth(screen_num as usize) {
-            Some(s) => s,
-            None => return Err(WmError::CouldNotAcquireScreen)
-        };
-        Ok(Wm {con: &con, root: screen.root()})
+        if let Some(screen) = setup.roots().nth(screen_num as usize) {
+            Ok(Wm {con: &con, root: screen.root()})
+        } else {
+            Err(WmError::CouldNotAcquireScreen)
+        }
     }
 
     // register and get back atoms
@@ -35,12 +58,13 @@ impl<'a> Wm<'a> {
         let mut cookies = Vec::with_capacity(names.len());
         let mut res = Vec::with_capacity(names.len());
         for name in names {
-            cookies.push(intern_atom(&self.con, false, name));
+            cookies.push((intern_atom(self.con, false, name), name));
         }
-        for cookie in cookies {
+        for (cookie, name) in cookies {
             match cookie.get_reply() {
                 Ok(r) => res.push(r.atom()),
-                Err(_) => return Err(WmError::CouldNotRegisterAtom)
+                Err(_) =>
+                    return Err(WmError::CouldNotRegisterAtom(name.to_string()))
             }
         }
         Ok(res)
@@ -55,22 +79,22 @@ impl<'a> Wm<'a> {
             | EVENT_MASK_PROPERTY_CHANGE
             | EVENT_MASK_BUTTON_PRESS;
         match change_window_attributes_checked(
-            &self.con, self.root, &[(CW_EVENT_MASK, values)]).request_check() {
+            self.con, self.root, &[(CW_EVENT_MASK, values)]).request_check() {
             Ok(()) => Ok(()),
             Err(_) => Err(WmError::OtherWmRunning)
         }
     }
 
     // main loop: wait for events, handle them (TODO)
-    pub fn run(&self) {
+    pub fn run(&self) -> Result<(), WmError> {
         loop {
             self.con.flush();
             if let Err(_) = self.con.has_error() {
-                panic!("Connection interrupted!");
+                return Err(WmError::ConnectionInterrupted);
             }
             match self.con.wait_for_event() {
                 Some(ev) => println!("Event recieved"),
-                None => panic!("I/O error!")
+                None => return Err(WmError::IOError)
             }
         }
     }
@@ -80,19 +104,22 @@ fn main() {
     // new connection to X server
     let (con, screen_num) = match Connection::connect(None) {
         Ok(c) => c,
-        Err(_) => panic!("Could not connect")
+        Err(e) => WmError::CouldNotConnect(e).handle()
     };
     // wm init
     let wm = match Wm::new(&con, screen_num) {
         Ok(w) => w,
-        Err(_) => {
-            println!("error.");
-            exit(1);
-        }
+        Err(e) => e.handle()
     };
     // atom setup
     let atoms = wm.get_atoms(vec!["WM_PROTOCOLS", "WM_DELETE_WINDOWS",
                              "WM_STATE", "WM_TAKE_FOCUS"]);
-    wm.register();
-    wm.run();
+    // register as a window manager and fail if another WM is running
+    if let Err(e) = wm.register() {
+        e.handle();
+    }
+    // main loop
+    if let Err(e) = wm.run() {
+        e.handle();
+    }
 }
