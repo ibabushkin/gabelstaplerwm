c5b082347f4ae70149c58fc11cc32b8802da2f91
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-04-14 09:04:09 +0200

    Added fixed dual stack layout

--- src/wm/layout.rs
+++ src/wm/layout.rs
@@ -166,19 +166,20 @@ impl Layout for HStack {
 
 // the dual stack layout
 // +-+---+-+
-// | |   | | A: left stack, hidden if num_windows <= 2
+// | |   | | A: left stack, hidden if fixed=false and num_windows <= 2
 // |A| B |C| B: master window
-// | |   | | C: right stack, hidden if num_windows <= 1
+// | |   | | C: right stack, hidden if fixed=false and num_windows <= 1
 // +-+---+-+
 // new slaves get added to the right stack,
 // so num_slaves_left <= num_slaves_right
 pub struct DStack {
     master_factor: u8, // percent
+    fixed: bool,
 }
 
 impl DStack {
     pub fn default() -> DStack {
-        DStack {master_factor: 50}
+        DStack {master_factor: 50, fixed: true}
     }
 }
 
@@ -192,19 +193,20 @@ impl Layout for DStack {
         } else {
             self.master_factor as u16 * screen.width / 100
         };
-        if num_windows == 1 {
+        if num_windows == 1 && !self.fixed {
             // one window only - fullscreen
             res.push(Some(Geometry {x: 0, y: 0,
                 width: screen.width, height: screen.height}));
         } else {
             let slave_width = (screen.width - master_width) / 2;
             // setup two slave stacks if needed
-            let (master_x, slave_right_x) = if num_windows == 2 {
-                (0, master_width) // no left stack - no shift
-            } else {
-                // shift master + right stack
-                (slave_width, slave_width + master_width)
-            };
+            let (master_x, slave_right_x) =
+                if num_windows == 2 && !self.fixed {
+                    (0, master_width) // no left stack - no shift
+                } else {
+                    // shift master + right stack
+                    (slave_width, slave_width + master_width)
+                };
             // master window
             res.push(Some(Geometry {x: master_x, y: 0,
                 width: master_width, height: screen.height}));
@@ -220,10 +222,11 @@ impl Layout for DStack {
                 }
             }
             let num_right_slaves = num_windows - 1 - num_left_slaves;
-            let slave_height_right = screen.height / num_right_slaves as u16;
             if num_right_slaves > 0 {
                 // if no left stack is present, the right
                 // stack can be made wider to avoid wasting space
+                let slave_height_right =
+                    screen.height / num_right_slaves as u16;
                 let width = if num_left_slaves == 0 {
                     screen.width - master_width
                 } else {
