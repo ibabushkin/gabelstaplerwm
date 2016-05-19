01f914e79400a1889cf48cdd82c1b34d59ed1e2b
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-05-19 22:17:22 +0200

    Updated keygrabbing utility
    * proper name
    * more elegant exit

--- IDEAS.md
+++ IDEAS.md
@@ -12,7 +12,6 @@ That way, users can choose components and replace/extend at will.
 * tags work similarly to awesomeWM: N:M relationship between windows and tags
   * it's cheaper to delete them from history than perform all manipulations
     over it, so `Vec` is probably still the best idea
-* [ ] helper util to get keycodes (via X I assume?)
 * [ ] notification support (look how naughty does it)
 
 ## Short term stuff
--- src/bin/gabelstaplergrab.rs
+++ src/bin/gabelstaplergrab.rs
@@ -1,29 +1,45 @@
 extern crate xcb;
 
+use std::process::exit;
+
 use xcb::base::*;
 use xcb::xkb as xkb;
 use xcb::xproto as xproto;
 
 fn main() {
+    // create a new connection, exit on failure
     let (con, screen_num) = match Connection::connect(None) {
         Ok(c) => c,
-        Err(_) => panic!("Could not connect :(")
+        Err(_) => {
+            println!("Could not connect :(");
+            exit(1);
+        },
     };
     let root = match con.get_setup().roots().nth(screen_num as usize) {
         Some(screen) => screen.root(),
-        None => panic!("Could not get root window :/")
+        None => {
+            println!("Could not get root window :/");
+            exit(2);
+        },
     };
-    let _ = xproto::change_window_attributes(
+    if let Err(_) = xproto::change_window_attributes(
         &con, root, &[(xproto::CW_EVENT_MASK, xproto::EVENT_MASK_KEY_PRESS)])
-        .request_check();
+        .request_check() {
+        println!("Other WM is running ;(");
+        exit(3);
+    }
     loop {
         con.flush();
         if let Err(_) = con.has_error() {
-            panic!("Connection had an error :|");
+            println!("Connection had an error :|");
+            exit(4);
         }
         match con.wait_for_event() {
             Some(ev) => print_event(ev),
-            None => panic!("IO Error occured :O")
+            None => {
+                println!("IO Error occured :O");
+                exit(5);
+            },
         }
     }
 }
@@ -31,7 +47,7 @@ fn main() {
 fn print_event(event: GenericEvent) {
     if event.response_type() == xkb::STATE_NOTIFY {
         let ev: &xkb::StateNotifyEvent = cast_event(&event);
-        println!("key pressed. code: {}, mods: {} o.o",
+        println!("key pressed. code: {}, mods: {}",
                  ev.xkbType(),
                  ev.keycode());
     }
