641fa9d98b3d64f841e052ab9793ab288cb1eb0d
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-06-23 00:17:56 +0200

    Added rustdocs to `kbd.rs`

--- src/wm/kbd.rs
+++ src/wm/kbd.rs
@@ -8,33 +8,44 @@ use wm::window_system::WmCommand;
 
 // constants for easier modifier handling
 #[allow(dead_code)]
+/// Symbolic constant: no modifier pressed.
 pub const NO_MODIFIER: u8 = 0;
+/// Symbolic constant: shift modifier pressed.
 pub const SHIFT: u8 = 1;
 #[allow(dead_code)]
+/// Symbolic constant: capslock modifier activated.
 pub const CAPSLOCK: u8 = 2;
+/// Symbolic constant: control modifier pressed.
 pub const CTRL: u8 = 4;
 #[allow(dead_code)]
+/// Symbolic constant: alt modifier pressed.
 pub const ALT: u8 = 8;
 #[allow(dead_code)]
+/// Symbolic constant: numlock modifier activated.
 pub const NUMLOCK: u8 = 16;
 #[allow(dead_code)]
+/// Symbolic constant: windows/mod4 modifier pressed.
 pub const MOD4: u8 = 64;
+/// Symbolic constant: alt gr modifier pressed.
 pub const ALTGR: u8 = 136;
 
-// closure type of a callback function running on key press
+/// Closure type of a callback function running on key press.
 pub type KeyCallback = Box<Fn(&mut ClientSet, &mut TagStack) -> WmCommand>;
-// keybinding map
+/// Keybinding map.
 pub type Keybindings = HashMap<KeyPress, KeyCallback>;
 
-// a key has been pressed - keycode and modifier information
+/// a key has been pressed - keycode and modifier information.
 #[derive(Hash, Eq, PartialEq, Clone, Copy, Debug)]
 pub struct KeyPress {
-    pub code: u8,   // number representing key
-    pub mods: u8,   // number representing modifier combination
-    pub mode: Mode, // necessary mode for modal keybindings
+    /// Symbolic integer representing key.
+    pub code: u8,
+    /// Symbolic integer representing modifier combination.
+    pub mods: u8,
+    /// Necessary mode for modal keybindings.
+    pub mode: Mode,
 }
 
-// get a KeyPress struct from a StateNotifyEvent
+/// Get a `KeyPress` struct from a `StateNotifyEvent`
 pub fn from_key(event: &xkb::StateNotifyEvent, mode: Mode) -> KeyPress {
     KeyPress {
         code: event.xkbType(),
