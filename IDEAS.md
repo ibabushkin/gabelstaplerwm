a4f6f0aad15ea78e8b83d8ff528ec5369cb025bc
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-07-06 16:28:45 +0200

    Added proper documentation to the macros in `src/wm/util.rs`

--- IDEAS.md
+++ IDEAS.md
@@ -4,13 +4,12 @@ very simple to remove tags, bar(s), fancy layouts etc.
 That way, users can choose components and replace/extend at will.
 
 ## General component ideas
-* additional functionality is placed in additional modules, that don't need
+* additional functionality is placed in additional modules, that don't *need*
   to be compiled in (separate crates)
   * [ ] scratchpad or similar concept
   * [ ] mouse replacement
   * [ ] bar (probably not)
   * [ ] notification support (look how naughty does it)
-* communication protocol with the outside world
 
 ## Short term stuff
 * add proper error handling to `handle_map_request()` et al
--- src/wm/util.rs
+++ src/wm/util.rs
@@ -13,9 +13,51 @@ macro_rules! bind {
     }
 }
 
+/// # General design concepts behind the macros
+/// All macros to be passed to `bind!` share a common property:
+/// they allow for outputting (almost) arbitrary data after successful
+/// execution and before control is returned to the window manager's core.
+///
+/// This can be achieved in two ways, which are easier to demonstrate than
+/// to explain properly:
+///
+/// This returns a closure that pushes tagset zero and calls the closure
+/// on the client list and tag stack container to generate some object and
+/// print it to stdout:
+/// ```
+/// push_tagset!(0;; |c, s| ...)
+/// ```
+///
+/// This returns a closure that pushes tagset zero and prints some previously
+/// computed value or values to stdout:
+/// ```
+/// push_tagset!(0; some_value)
+/// push_tagset!(0; some_value; some_more_values_here)
+/// ```
+///
+/// These methods are different because the former allows to compute the output
+/// after the "real work" has been done by the callback closure generated from
+/// the macro, whereas the latter is more readable, but doesn't allow to do
+/// that. Note that in the case of multiple objects passed to the second form,
+/// they are printed on separate lines.
+///
+/// Another thing to remember: if the closure accepts multiple arguments, they
+/// are comma-separated and come before the semicolon(s), like so:
+/// ```
+/// some_macro!(param_1, param_2, ... , param_n; optional_output(s)/whatever)
+/// ```
+
 /// View a tagset by pushing it by index on the history stack.
 ///
 /// Returns a closure for use with `bind!`.
+///
+/// # Usage
+/// The `push_tagset!` macro expects the index of the tagset to be focused.
+/// The returned closure makes that tagset the current tagset and modifies
+/// the history stack accordingly.
+///
+/// As always, the last parameter(s) specify objects to be printed after
+/// completion of the action.
 macro_rules! push_tagset {
     ($index:expr;; $print:expr) => {
         |c, s| {
@@ -36,6 +78,13 @@ macro_rules! push_tagset {
 /// Toggle a tag on a client.
 ///
 /// Returns a closure for use with `bind!`.
+///
+/// # Usage
+/// The `toggle_tag!` macro expects a tag to be toggled on the currently
+/// focused client.
+///
+/// As always, the last parameter(s) specify objects to be printed after
+/// completion of the action.
 macro_rules! toggle_tag {
     ($tag:expr;; $print:expr) => {
         |c, s| s
@@ -64,6 +113,13 @@ macro_rules! toggle_tag {
 /// Toggle a tag on the current tagset.
 ///
 /// Returns a closure for use with `bind!`.
+///
+/// # Usage
+/// The `toggle_show_tag!` macro expects a tag to be toggled on the currently
+/// viewed tagset.
+///
+/// As always, the last parameter(s) specify objects to be printed after
+/// completion of the action.
 macro_rules! toggle_show_tag {
     ($tag:expr;; $print:expr) => {
         |c, s| s
@@ -90,6 +146,13 @@ macro_rules! toggle_show_tag {
 /// Move a client to a tag.
 ///
 /// Returns a closure for use with `bind!`.
+///
+/// # Usage
+/// The `move_to_tag!` macro expects one or more tags for the current client
+/// to be moved to.
+///
+/// As always, the last parameter(s) specify objects to be printed after
+/// completion of the action.
 macro_rules! move_to_tag {
     ($($tag:expr),*;; $print:expr) => {
         |c, s| s
@@ -98,6 +161,7 @@ macro_rules! move_to_tag {
             .and_then(|w| c.update_client(w, |mut cl| {
                 cl.set_tags(&[$($tag),*]);
                 println!("{}", $print(c, s));
+                // TODO: optimize for cases, where current tags are present
                 WmCommand::Redraw
             }))
             .unwrap_or(WmCommand::NoCommand)
@@ -109,6 +173,7 @@ macro_rules! move_to_tag {
             .and_then(|w| c.update_client(w, |mut cl| {
                 cl.set_tags(&[$($tag),*]);
                 $( println!("{}", $print); )*
+                // TODO: optimize for cases, where current tags are present
                 WmCommand::Redraw
             }))
             .unwrap_or(WmCommand::NoCommand)
@@ -118,6 +183,12 @@ macro_rules! move_to_tag {
 /// Focus a client using a closure.
 ///
 /// Returns a closure for use with `bind!`.
+///
+/// # Usage
+/// The `focus!` macro expects a closure determining the client to focus.
+///
+/// As always, the last parameter(s) specify objects to be printed after
+/// completion of the action.
 macro_rules! focus {
     ($func:expr;; $print:expr) => {
         |c, s| s
@@ -142,6 +213,13 @@ macro_rules! focus {
 /// Swap a client using a closure.
 ///
 /// Returns a closure for use with `bind!`.
+///
+/// # Usage
+/// The `swap!` macro expects a closure determnining the client to swap the
+/// current client with.
+///
+/// As always, the last parameter(s) specify objects to be printed after
+/// completion of the action.
 macro_rules! swap {
     ($func:expr;; $print:expr) => {
         |c, s| s
@@ -166,6 +244,18 @@ macro_rules! swap {
 /// Edit the current layout via a `LayoutCommand`.
 ///
 /// Returns a closure for use with `bind!`.
+///
+/// # Usage
+/// The `edit_layout!` macro expects a `LayoutCommand` and passes it to the
+/// layout of the currently viewed tagset. This allows for modification of
+/// layout parameters from keybindings. However, there is no guarantee that
+/// a specific layout reacts to a specific `LayoutCommand`, as it may be
+/// meaningless in some contexts. For example, the `Monocle` layout doesn't
+/// have a notion of a master factor, so the corresponding `LayoutCommand`s
+/// have no effect on it.
+///
+/// As always, the last parameter(s) specify objects to be printed after
+/// completion of the action.
 macro_rules! edit_layout {
     ($cmd:expr;; $print:expr) => {
         |_, s| s
