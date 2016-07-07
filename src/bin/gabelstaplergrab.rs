2d4c5aa4175a98722c51ce4f0ad1bf171191ef00
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-07-07 23:29:40 +0200

    Added logger to `gabelstaplergrab`

--- src/bin/gabelstaplergrab.rs
+++ src/bin/gabelstaplergrab.rs
@@ -1,5 +1,9 @@
 extern crate xcb;
 
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+
 use std::process::exit;
 
 use xcb::base::*;
@@ -11,37 +15,42 @@ use xcb::xproto as xproto;
 /// Establish a connection to the X server and print `KeyPress`es that take
 /// place.
 fn main() {
+    // logger setup
+    if env_logger::init().is_err() {
+        println!("ERROR:main: could not setup logger");
+    }
+
     // create a new connection, exit on failure
     let (con, screen_num) = match Connection::connect(None) {
         Ok(c) => c,
         Err(_) => {
-            println!("Could not connect :(");
+            error!("could not connect");
             exit(1);
         },
     };
     let root = match con.get_setup().roots().nth(screen_num as usize) {
         Some(screen) => screen.root(),
         None => {
-            println!("Could not get root window :/");
+            error!("could not get root window");
             exit(2);
         },
     };
-    if let Err(_) = xproto::change_window_attributes(
+    if xproto::change_window_attributes(
         &con, root, &[(xproto::CW_EVENT_MASK, xproto::EVENT_MASK_KEY_PRESS)])
-        .request_check() {
-        println!("Other WM is running ;(");
+        .request_check().is_err() {
+        error!("other window manager running");
         exit(3);
     }
     loop {
         con.flush();
         if let Err(_) = con.has_error() {
-            println!("Connection had an error :|");
+            error!("connection error");
             exit(4);
         }
         match con.wait_for_event() {
             Some(ev) => print_event(ev),
             None => {
-                println!("IO Error occured :O");
+                error!("i/o error occured");
                 exit(5);
             },
         }
@@ -52,7 +61,7 @@ fn main() {
 fn print_event(event: GenericEvent) {
     if event.response_type() == xkb::STATE_NOTIFY {
         let ev: &xkb::StateNotifyEvent = cast_event(&event);
-        println!("key pressed. code: {}, mods: {}",
+        println!("key pressed: code: {}, mods: {}",
                  ev.xkbType(),
                  ev.keycode());
     }
