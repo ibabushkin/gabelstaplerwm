648e26c7dfec5211a3e356b325712ab8ccecac03
Inokentiy Babushkin <twk@twki.de>
2017-10-16 23:47:33 +0200

    First steps for a command input loop.

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+/*
+ * Copyright Inokentiy Babushkin and contributors (c) 2016-2017
+ *
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Inokentiy Babushkin nor the names of other
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+extern crate env_logger;
+extern crate libc;
+#[macro_use]
+extern crate log;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::os::unix::io::AsRawFd;
+
+fn setup_pollfd(fd: &File) -> libc::pollfd {
+    libc::pollfd {
+        fd: fd.as_raw_fd(),
+        events: libc::POLLIN,
+        revents: 0,
+    }
+}
+
+fn poll(fds: &mut [libc::pollfd]) -> bool {
+    let poll_res = unsafe {
+        libc::poll(fds.as_mut_ptr(), fds.len() as u64, -1)
+    };
+
+    poll_res > 0
+}
+
+fn main() {
+    // fine to unwrap, as this is the only time we call `init`.
+    env_logger::init().unwrap();
+    info!("initialized logger");
+}
